73ff161cec2d46939bf5506b94f609864365de2f
Momo <mynameisjoyg@gmail.com>
2026-09-19 21:41:38 +0800

    ;rgb:bfbf/bfbf/bfbfrgb:0000/0000/0000
    This reverts commit dedd3b6ff04e66082faedb2bb1314b688661b595.
    
    	modified:   app/src/androidTest/java/com/joyg/quizhero/QuizSelectionTest.kt
    	modified:   app/src/main/java/com/joyg/quizhero/QuizActivity.kt
    
    	I forget to add fixes # 4.

--- app/src/androidTest/java/com/joyg/quizhero/QuizSelectionTest.kt
+++ app/src/androidTest/java/com/joyg/quizhero/QuizSelectionTest.kt
@@ -48,12 +48,6 @@ class QuizSelectionTest {
 
     @Test
     fun testSelectSpinnerItemsAndStartQuiz_DoesNotCrash() {
-        //Test Chinese, volume 5, chapter 9.
-        testSpinnerBySubjectVolumeChapter(1,4, 8)
-
-        //Test Chinese, volume 6, chapter 5.
-        testSpinnerBySubjectVolumeChapter(1,5, 4)
-
         //Test English, volume1, chapter 1~6.
         for(chapter in 0 until 6){
             testSpinnerBySubjectVolumeChapter(0,0,chapter)
@@ -70,14 +64,6 @@ class QuizSelectionTest {
                 testSpinnerBySubjectVolumeChapter(0,volume, chapter)
             }
         }
-
-        //Test Chinese, volume1~4, chapter 1~10.
-        for(volume in 0 until 5){
-            for(chapter in 0 until 9){
-                testSpinnerBySubjectVolumeChapter(1,volume, chapter)
-            }
-        }
-
     }
 
     private fun testSpinnerBySubjectVolumeChapter(sub: Int, vol: Int, cha: Int){
