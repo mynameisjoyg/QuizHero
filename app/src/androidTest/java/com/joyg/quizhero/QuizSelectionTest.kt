08a8bf0ef425f8afaac6a3623ce6c4e824b19926
Joey <mynameisjoyg@gmail.com>
2026-09-19 16:46:15 +0800

    Try to test all English volume and chapter, but it will failed sometimes.

--- app/src/androidTest/java/com/joyg/quizhero/QuizSelectionTest.kt
+++ app/src/androidTest/java/com/joyg/quizhero/QuizSelectionTest.kt
@@ -1,7 +1,9 @@
 package com.joyg.quizhero // 請改成你專案實際的 package 名稱
 
+import android.os.SystemClock
 import android.util.Log
 import android.widget.ArrayAdapter
+import androidx.test.espresso.Espresso
 import androidx.test.espresso.Espresso.onData
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
@@ -46,24 +48,53 @@ class QuizSelectionTest {
 
     @Test
     fun testSelectSpinnerItemsAndStartQuiz_DoesNotCrash() {
+        //Test English, volume1, chapter 1~6.
+        for(chapter in 0 until 6){
+            testSpinnerBySubjectVolumeChapter(0,0,chapter)
+        }
 
+        //Test English, volume2, chapter 1~4.
+        for(chapter in 0 until 4){
+            testSpinnerBySubjectVolumeChapter(0,1,chapter)
+        }
+
+        //Test English, volume3~5, chapter 1~6.
+        for(volume in 2 until 5){
+            for(chapter in 0 until 6){
+                testSpinnerBySubjectVolumeChapter(0,volume, chapter)
+            }
+        }
+    }
+
+    private fun testSpinnerBySubjectVolumeChapter(sub: Int, vol: Int, cha: Int){
         // 1. 選擇 Subject (科目 Spinner)
         onView(withId(R.id.sp_subject)).perform(click())
         // 假設 Spinner 內容是 String，這裡模擬點擊第二個選項（或指定字串）
-        onData(allOf(`is`(instanceOf(String::class.java)))).atPosition(0).perform(click())
+        onData(allOf(`is`(instanceOf(String::class.java)))).atPosition(sub).perform(click())
 
         // 2. 選擇 Volume (冊別 Spinner)
         onView(withId(R.id.sp_volume)).perform(click())
-        onData(allOf(`is`(instanceOf(String::class.java)))).atPosition(0).perform(click())
+        onData(allOf(`is`(instanceOf(String::class.java)))).atPosition(vol).perform(click())
 
         // 3. 選擇 Chapter (章節 Spinner)
         onView(withId(R.id.sp_chapter)).perform(click())
-        onData(allOf(`is`(instanceOf(String::class.java)))).atPosition(0).perform(click())
+        onData(allOf(`is`(instanceOf(String::class.java)))).atPosition(cha).perform(click())
 
         // 4. 點擊開始測驗按鈕 (請替換為你的按鈕 ID，例如 btn_start_quiz)
         onView(withId(R.id.bt_exam)).perform(click())
 
         // 5. 驗證：確認有發出跳轉至 QuizActivity 的 Intent 且沒有閃退
         intended(hasComponent(QuizActivity::class.java.name))
+
+        // 等待 5 秒
+        //SystemClock.sleep(3000)
+        onView(withId(R.id.tv_question)).check(matches(isDisplayed()))
+
+        // 6. 重要：返回 MainActivity，以便進行下一個迴圈測試
+        Espresso.pressBack()
+
+        // 清除過往的 Intent 紀錄，讓下一次迴圈重新計算
+        Intents.release()
+        Intents.init()
     }
 }
