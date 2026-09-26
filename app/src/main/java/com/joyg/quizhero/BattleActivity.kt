425e14cd5921faa5c38785b810ca085bd06ba726
Momo <mynameisjoyg@gmail.com>
2026-09-26 09:21:31 +0800

    Solve the problem that the option's string will not show correctly when string is too long.

--- app/src/main/java/com/joyg/quizhero/BattleActivity.kt
+++ app/src/main/java/com/joyg/quizhero/BattleActivity.kt
@@ -7,6 +7,8 @@ import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 import com.google.firebase.Firebase
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.firestore
@@ -48,8 +50,7 @@ class BattleActivity : AppCompatActivity() {
 
     // 1. 宣告 FirebaseFirestore 變數
     private lateinit var db: FirebaseFirestore
-
-
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_battle)
